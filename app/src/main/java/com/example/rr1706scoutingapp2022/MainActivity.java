81835ebfde7100a83f27991f6c37bcc6e313c09e
UltraDagon <43974011+UltraDagon@users.noreply.github.com>
2022-01-19 18:39:51 -0600

    Use this to build off of. Added open/close function usable for PREGAME and ENDGAME windows. Format for ids and stuff: Layouts: LAYOUTNAME, not layouts: parent_name_object. Ex: pregame_open_button or submit_text. parent being the parent layout, unless it's in base layout, then its just blank. Also you dont need the "view" part of TextView or ImageView for ids.

--- app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
+++ app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.rr1706scoutingapp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,42 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        //Constraints
+        final ConstraintLayout PREGAME = findViewById(R.id.PREGAME);
+
+        //Lines
+
+        //Buttons
+        final Button pregame_open_button = findViewById(R.id.pregame_open_button);
+        final Button pregame_close_button = findViewById(R.id.pregame_close_button);
+        //ImageViews
+
+        //TextViews
+
+        //EditTexts
+
+        //CheckBoxes
+
+        //Spinners
+
+        //Other elements
+
+        //Set invisible/visible/tinted elements
+
+        //Pregame
+        pregame_open_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PREGAME.setVisibility(View.VISIBLE);
+            }
+        });
+
+        pregame_close_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PREGAME.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
