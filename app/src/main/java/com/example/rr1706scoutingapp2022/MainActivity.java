17fc4e26eedfc772ac88739c12882d17391a187f
Connor Fogarty <connorfogarty26@gmail.com>
2022-01-25 19:47:21 -0600

    Worked on functionality - Bryce

--- app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
+++ app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.rr1706scoutingapp2022;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Constraints
-        final ConstraintLayout PREGAME = findViewById(R.id.PREGAME);
-
+        //final ConstraintLayout PREGAME = findViewById(R.id.PREGAME);
+        final ConstraintLayout Background = findViewById(R.id.Background);
         //Lines
 
         //Buttons
-        final Button pregame_open_button = findViewById(R.id.pregame_open_button);
-        final Button pregame_close_button = findViewById(R.id.pregame_close_button);
+        //final Button pregame_open_button = findViewById(R.id.pregame_open_button);
+        //final Button pregame_close_button = findViewById(R.id.pregame_close_button);
+        final Button Blue_Alliance = findViewById(R.id.Blue_Alliance);
+
         //ImageViews
 
         //TextViews
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         //Set invisible/visible/tinted elements
 
         //Pregame
-        pregame_open_button.setOnClickListener(new View.OnClickListener() {
+        /*pregame_open_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PREGAME.setVisibility(View.VISIBLE);
@@ -55,6 +60,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 PREGAME.setVisibility(View.INVISIBLE);
             }
+        });*/
+
+        Blue_Alliance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Background.setBackgroundColor(Color.argb(255, 223, 223, 255));
+            }
         });
+    }
+
+    public void hideKeyboard(View view)
+    {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
