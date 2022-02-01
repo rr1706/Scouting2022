fbffe18c9c730b8b5bafa31031a126716b78f2fc
Connor Fogarty <connorfogarty26@gmail.com>
2022-01-31 19:10:29 -0600

    Made Button. Button work. Yay - Bryce

--- app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
+++ app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.rr1706scoutingapp2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Group;
 
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.transition.Fade;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -26,13 +28,15 @@ public class MainActivity extends AppCompatActivity {
         //Constraints
         //final ConstraintLayout PREGAME = findViewById(R.id.PREGAME);
         final ConstraintLayout Background = findViewById(R.id.Background);
+        final ConstraintLayout Pregame = findViewById(R.id.Pregame);
         //Lines
 
         //Buttons
         //final Button pregame_open_button = findViewById(R.id.pregame_open_button);
         //final Button pregame_close_button = findViewById(R.id.pregame_close_button);
         final Button Blue_Alliance = findViewById(R.id.Blue_Alliance);
-
+        final Button Red_Alliance = findViewById(R.id.Red_Alliance);
+        final Button Pregame_Box = findViewById(R.id.Pregame_Box);
         //ImageViews
 
         //TextViews
@@ -47,10 +51,13 @@ public class MainActivity extends AppCompatActivity {
 
         //Set invisible/visible/tinted elements
 
+        //Groups
+
+
         //Pregame
         /*pregame_open_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {"
                 PREGAME.setVisibility(View.VISIBLE);
             }
         });
@@ -61,18 +68,33 @@ public class MainActivity extends AppCompatActivity {
                 PREGAME.setVisibility(View.INVISIBLE);
             }
         });*/
+        Pregame_Box.setOnClickListener(view -> {
+            if(Pregame.getVisibility()==View.INVISIBLE)
+                Pregame.setVisibility(View.VISIBLE);
+            else if(Pregame.getVisibility()==View.VISIBLE)
+                Pregame.setVisibility(View.INVISIBLE);
+        });
 
         Blue_Alliance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Background.setBackgroundColor(Color.argb(255, 223, 223, 255));
+                Pregame.setBackgroundColor(Color.argb(255, 127, 127, 247));
             }
         });
+
+
+        Red_Alliance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Background.setBackgroundColor(Color.argb(255, 255, 223, 223));
+                Pregame.setBackgroundColor(Color.argb(255, 247, 127, 127));
+        }});
     }
 
     public void hideKeyboard(View view)
     {
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
-    }
+    };
 }
