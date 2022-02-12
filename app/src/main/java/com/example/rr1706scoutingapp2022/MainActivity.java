34c335e03f506c391705c09d9a29bdcb17a3bd8e
Connor Fogarty <connorfogarty26@gmail.com>
2022-02-12 12:36:50 -0600

    Added more functionality, and finished adding in all of the buttons for data. I am working on submitting, but this will be the stable version that is functionally working.  - X

--- app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
+++ app/src/main/java/com/example/rr1706scoutingapp2022/MainActivity.java
@@ -8,8 +8,10 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.text.InputType;
 import android.transition.Fade;
 import android.util.Log;
 import android.view.View;
@@ -17,8 +19,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
         int ds_cooldown = 0; //ds_cooldown is the cool down for the data_submitted animation
         int team;
         int round;
+        int missedScore;
         int autoLowerScore;
         int autoUpperScore;
         int teleopUpperScore;
@@ -48,12 +54,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
         //Ints
-
+        final Spinner speed = findViewById(R.id.robotSpeed);
+        final Spinner shotDistance = findViewById(R.id.shotDistance);
+        final Spinner endgame_results = findViewById(R.id.endgameResults);
+        final Spinner violations = findViewById(R.id.violations);
+        final Spinner climbResult = findViewById(R.id.endgameClimb);
         //Constraints
         //final ConstraintLayout PREGAME = findViewById(R.id.PREGAME);
         final ConstraintLayout Background = findViewById(R.id.Background);
@@ -69,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText notes = findViewById(R.id.notes);
         final TextView auto_upper_text = findViewById(R.id.auto_upper_text);
         final TextView auto_lower_text = findViewById(R.id.auto_lower_text);
+        final TextView missedShotsText = findViewById(R.id.missedShotsText);
         final TextView teleop_upper_text = findViewById(R.id.teleop_upper_text);
         final TextView teleop_lower_text = findViewById(R.id.teleop_lower_text);
         data_submitted.setVisibility(View.INVISIBLE);
@@ -78,9 +89,12 @@ public class MainActivity extends AppCompatActivity {
         final Button Blue_Alliance = findViewById(R.id.Blue_Alliance);
         final Button Red_Alliance = findViewById(R.id.Red_Alliance);
         final Button Pregame_Box = findViewById(R.id.Pregame_Box);
+        final Button pregame_close = findViewById(R.id.pregame_close);
         final Button Endgame_Box = findViewById(R.id.Endgame_Box);
         final Button no_show = findViewById(R.id.no_show);
         final Button submit = findViewById(R.id.submit);
+        final ImageView missedShotsPositive = findViewById(R.id.missedShotsPosititve);
+        final ImageView missedShotsMinus = findViewById(R.id.missedShotsMinus);
         final ImageView auto_upper_plus = findViewById(R.id.auto_upper_plus);
         final ImageView auto_upper_minus = findViewById(R.id.auto_upper_minus);
         final ImageView auto_lower_plus = findViewById(R.id.auto_lower_plus);
@@ -89,9 +103,11 @@ public class MainActivity extends AppCompatActivity {
         final ImageView teleop_upper_minus = findViewById(R.id.teleop_upper_minus);
         final ImageView teleop_lower_plus = findViewById(R.id.teleop_lower_plus);
         final ImageView teleop_lower_minus = findViewById(R.id.teleop_lower_minus);
+        final Switch auto_no_auto = findViewById(R.id.noAutoSwitch);
+        final Switch autoMovement = findViewById(R.id.autoMovementSwitch);
         //No Show
         final DialogInterface.OnClickListener NoShowDialog = (dialog, which) -> {
-            switch (which){
+            switch (which) {
                 case DialogInterface.BUTTON_POSITIVE:
                     data_submitted.setImageResource(R.drawable.check);
                     data_submitted.setVisibility(View.VISIBLE);
@@ -130,14 +146,24 @@ public class MainActivity extends AppCompatActivity {
 
             //Special handling
 
-            if (alliance == "none") { submitError += " No Alliance,"; }
-            if (name_input.getText().toString().equals("")) { submitError += " No Name,"; }
-            if (team_input.getText().toString().equals("")) { submitError += " No Team#,"; }
-            if (round_input.getText().toString().equals("")) { submitError += " No Round#,"; }
-            if (!submitError.equals("")) { submitError = submitError.substring(0,submitError.length()-1)+"."; }
+            if (alliance == "none") {
+                submitError += " No Alliance,";
+            }
+            if (name_input.getText().toString().equals("")) {
+                submitError += " No Name,";
+            }
+            if (team_input.getText().toString().equals("")) {
+                submitError += " No Team#,";
+            }
+            if (round_input.getText().toString().equals("")) {
+                submitError += " No Round#,";
+            }
+            if (!submitError.equals("")) {
+                submitError = submitError.substring(0, submitError.length() - 1) + ".";
+            }
 
             if (!(submitError.equals(""))) {
-                Toast.makeText(getApplicationContext(), "Submit Error:"+submitError, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Submit Error:" + submitError, Toast.LENGTH_LONG).show();
 
                 data_submitted.setVisibility(View.VISIBLE);
                 data_submitted.setImageResource(R.drawable.x);
@@ -160,49 +186,81 @@ public class MainActivity extends AppCompatActivity {
                 }
                 data_submitted.post(() -> {
                     //data_submitted stuff
-                    if (ds_cooldown > 0) { ds_cooldown--; }
+                    if (ds_cooldown > 0) {
+                        ds_cooldown--;
+                    }
 
-                    if (ds_cooldown == 0) { data_submitted.setVisibility(View.GONE); }
+                    if (ds_cooldown == 0) {
+                        data_submitted.setVisibility(View.GONE);
+                    }
                 });
             }
         };
         Thread myThread = new Thread(myRunnable);
         myThread.start();
         auto_upper_plus.setOnClickListener(v -> {
-            if (autoUpperScore < 99) { autoUpperScore++; }
+            if (autoUpperScore < 99) {
+                autoUpperScore++;
+            }
             auto_upper_text.setText(Integer.toString(autoUpperScore));
         });
         auto_upper_minus.setOnClickListener(v -> {
-            if (autoUpperScore > 0) { autoUpperScore--; }
+            if (autoUpperScore > 0) {
+                autoUpperScore--;
+            }
             auto_upper_text.setText(Integer.toString(autoUpperScore));
         });
 
         auto_lower_plus.setOnClickListener(v -> {
-            if (autoLowerScore < 99) { autoLowerScore++; }
+            if (autoLowerScore < 99) {
+                autoLowerScore++;
+            }
             auto_lower_text.setText(Integer.toString(autoLowerScore));
         });
         auto_lower_minus.setOnClickListener(v -> {
-            if (autoLowerScore > 0) { autoLowerScore--; }
+            if (autoLowerScore > 0) {
+                autoLowerScore--;
+            }
             auto_lower_text.setText(Integer.toString(autoLowerScore));
         });
 
 
         teleop_upper_plus.setOnClickListener(v -> {
-            if (teleopUpperScore < 99) { teleopUpperScore++; }
+            if (teleopUpperScore < 99) {
+                teleopUpperScore++;
+            }
             teleop_upper_text.setText(Integer.toString(teleopUpperScore));
         });
         teleop_upper_minus.setOnClickListener(v -> {
-            if (teleopUpperScore > 0) { teleopUpperScore--; }
+            if (teleopUpperScore > 0) {
+                teleopUpperScore--;
+            }
             teleop_upper_text.setText(Integer.toString(teleopUpperScore));
         });
 
         teleop_lower_plus.setOnClickListener(v -> {
-            if (teleopLowerScore < 99) { teleopLowerScore++; }
+            if (teleopLowerScore < 99) {
+                teleopLowerScore++;
+            }
             teleop_lower_text.setText(Integer.toString(teleopLowerScore));
         });
         teleop_lower_minus.setOnClickListener(v -> {
-            if (teleopLowerScore > 0) { teleopLowerScore--; }
+            if (teleopLowerScore > 0) {
+                teleopLowerScore--;
+            }
             teleop_lower_text.setText(Integer.toString(teleopLowerScore));
+        });
+        missedShotsPositive.setOnClickListener(v -> {
+            if (missedScore < 99) {
+                missedScore++;
+            }
+            missedShotsText.setText(Integer.toString(missedScore));
+        });
+        missedShotsMinus.setOnClickListener(v -> {
+            if (missedScore > 0) {
+                missedScore--;
+            }
+            missedShotsText.setText(Integer.toString(missedScore));
         });
         //ImageViews
 
@@ -220,25 +278,26 @@ public class MainActivity extends AppCompatActivity {
 
         //Groups
 
-
         //Pregame
+        pregame_close.setOnClickListener(view -> {
+            Pregame.setVisibility(View.INVISIBLE);
+        });
+
         Endgame.setVisibility(View.INVISIBLE);
         Pregame_Box.setOnClickListener(view -> {
-            if(Pregame.getVisibility()==View.INVISIBLE) {
+            if (Pregame.getVisibility() == View.INVISIBLE) {
                 Endgame.setVisibility(View.INVISIBLE);
                 Pregame.setVisibility(View.VISIBLE);
-            }
-            else if(Pregame.getVisibility()==View.VISIBLE) {
+            } else if (Pregame.getVisibility() == View.VISIBLE) {
                 Pregame.setVisibility(View.INVISIBLE);
                 Endgame.setVisibility(View.INVISIBLE);
             }
         });
         Endgame_Box.setOnClickListener(view -> {
-            if(Endgame.getVisibility()==View.VISIBLE) {
+            if (Endgame.getVisibility() == View.VISIBLE) {
                 Pregame.setVisibility(View.INVISIBLE);
                 Endgame.setVisibility(View.INVISIBLE);
-            }
-            else if(Endgame.getVisibility()==View.INVISIBLE) {
+            } else if (Endgame.getVisibility() == View.INVISIBLE) {
                 Endgame.setVisibility(View.VISIBLE);
                 Pregame.setVisibility(View.INVISIBLE);
             }
@@ -247,7 +306,7 @@ public class MainActivity extends AppCompatActivity {
             Background.setBackgroundResource(R.drawable.blueappbackground);
             Pregame.setBackgroundColor(Color.argb(255, 127, 127, 247));
             Endgame.setBackgroundColor(Color.argb(255, 127, 127, 247));
-            alliance="blue";
+            alliance = "blue";
         });
 
 
@@ -255,8 +314,96 @@ public class MainActivity extends AppCompatActivity {
             Background.setBackgroundResource(R.drawable.redappbackground);
             Pregame.setBackgroundColor(Color.argb(255, 247, 127, 127));
             Endgame.setBackgroundColor(Color.argb(255, 247, 127, 127));
-            alliance="red";
-    });
+            alliance = "red";
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String submitError = "";
+                String robotErrors = "";
+                String genPos;
+                SimpleDateFormat time = new SimpleDateFormat("dd-HHmmss", Locale.getDefault());
+                int team;
+                int round;
+
+                //Special handling
+                if (team_input.getText().toString().equals("")) { team = -1; }
+                else { team = Integer.parseInt(team_input.getText().toString()); }
+
+                if (round_input.getText().toString().equals("")) { round = -1; }
+                else { round = Integer.parseInt(round_input.getText().toString()); }
+
+                if (alliance == "none") { submitError += " No Alliance,"; }
+                if (name_input.getText().toString().equals("")) { submitError += " No Name,"; }
+                if (speed.getSelectedItem().toString().equals("No Input")) { submitError += " No Speed,"; }
+                if (endgame_results.getSelectedItem().toString().equals("No Input")) { submitError += " No Results,"; }
+                if (violations.getSelectedItem().toString().equals("No Input")) { submitError += " No Violations,"; }
+                if (climbResult.getSelectedItem().toString().equals("No Input")) { submitError += " No Climb Result,"; }
+                if (shotDistance.getSelectedItem().toString().equals("No Input")) { submitError += " No Shot Distance,"; }
+                if (team_input.getText().toString().equals("")) { submitError += " No Team#,"; }
+                if (round_input.getText().toString().equals("")) { submitError += " No Round#,"; }
+                if (!submitError.equals("")) { submitError = submitError.substring(0,submitError.length()-1)+"."; }
+
+                if (!(submitError.equals(""))) {
+                    Toast.makeText(getApplicationContext(), "Submit Error:"+submitError, Toast.LENGTH_LONG).show();
+
+                    data_submitted.setVisibility(View.VISIBLE);
+                    data_submitted.setImageResource(R.drawable.x);
+                    ds_cooldown = 150;
+                } else {
+                    data_submitted.setVisibility(View.VISIBLE);
+                    data_submitted.setImageResource(R.drawable.check);
+                    ds_cooldown = 150; //Makes the check mark appear
+
+                    //Save data for transfer
+                    File dir = getDataDirectory();
+
+                    try {
+                        File myFile = new File(dir, team + "_" + round + "_" + time.format(new Date()) + ".txt");
+                        FileOutputStream fOut = new FileOutputStream(myFile, true);
+                        PrintWriter myOutWriter = new PrintWriter(new OutputStreamWriter(fOut));
+
+                        myOutWriter.println("Scouter: "+name_input.getText());
+                        myOutWriter.println("Team: "+team);
+                        myOutWriter.println("Timestamp: "+time.format(new Date()));
+                        myOutWriter.println("Match: "+round);
+                        myOutWriter.println("Alliance: "+alliance);
+                        myOutWriter.println("Speed: "+speed.getSelectedItem().toString());
+                        //Robot errors handling
+                        if (robotErrors.equals("FALSE")) { robotErrors = "None"; }
+                        myOutWriter.println("Robot Errors: "+robotErrors);
+                        myOutWriter.println("Auto Top Score: "+autoUpperScore);
+                        myOutWriter.println("Auto Bottom Score: "+autoLowerScore);
+                        myOutWriter.println("No Auto: "+auto_no_auto.isChecked());
+                        myOutWriter.println("Passed Init Line: "+autoMovement.isChecked());
+                        myOutWriter.println("Teleop Top Score: "+teleopUpperScore);
+                        myOutWriter.println("Teleop Bottom Score: "+teleopLowerScore);
+                        //Rotation control handling
+                        //Generator position handling
+                        myOutWriter.println("Endgame: "+endgame_results);
+                        myOutWriter.println("Results: "+endgame_results.getSelectedItem());
+                        myOutWriter.println("Notes: "+notes.getText());
+
+                        myOutWriter.flush();
+                        myOutWriter.close();
+                        fOut.close();
+
+                        Toast.makeText(getApplicationContext(), "Data Submitted!", Toast.LENGTH_SHORT).show();
+
+                    } catch (IOException e) {
+                        Toast.makeText(getApplicationContext(), "Data Submission Failed! (Tell scouting)", Toast.LENGTH_SHORT).show();
+                        Log.e("Exception", "File write failed: " + e.toString());
+                    }
+
+                    //Reset vars
+                    teleopLowerScore = 0;
+                    teleopUpperScore = 0;
+                    autoLowerScore = 0;
+                    autoUpperScore = 0;
+
+                }
+            }
+        });
     }
     private File getDataDirectory() {
         File directory = Environment.getExternalStorageDirectory();
@@ -266,7 +413,12 @@ public class MainActivity extends AppCompatActivity {
     }
     public void hideKeyboard(View view)
     {
-        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        InputMethodManager inputMethodManager = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            inputMethodManager = (InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.CUPCAKE) {
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
